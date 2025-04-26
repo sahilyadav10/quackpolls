c508fd748f1ed4018d2ac971fd4c6033f88ec0d5
Sahil Yadav <10sahil11@gmail.com>
2025-04-26 09:49:21 +0530

    add pill variants

--- src/components/generic/Pill.tsx
+++ src/components/generic/Pill.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 type PillProps = {
   children: ReactNode;
-  variant?: "default" | "primary" | "secondary";
+  variant?: "default" | "primary" | "secondary" | "active" | "inactive";
   className?: string;
 };
 
@@ -11,8 +11,9 @@ const baseStyles = "rounded-full px-3 py-1 w-fit";
 const typeStyles = {
   default: "text-neutral-900 dark:text-neutral-50 border border-primary/30",
   primary: "bg-primary/20 text-primary",
-  secondary:
-    "bg-neutral-100 text-neutral-800 dark:bg-neutral-700 dark:text-neutral-200",
+  secondary: "bg-neutral-100 text-neutral-800",
+  active: "bg-green-100 text-green-600",
+  inactive: "bg-neutral-100 text-neutral-400",
 };
 
 export default function Pill({
