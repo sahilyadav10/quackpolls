db35cc22eb66e10199a83f7a012547c81b940c97
Sahil Yadav <10sahil11@gmail.com>
2025-04-30 01:43:31 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { websiteMetadata } from "@/utils/metadata";
 import { ReduxProvider } from "@/components/providers/ReduxProvider";
 import { ReactQueryProvider } from "@/components/providers/ReactQueryProvider";
 import Banner from "@/components/app/Banner";
+import { PostHogProvider } from "@/context/Posthog";
 
 export const metadata: Metadata = websiteMetadata;
 
@@ -25,18 +26,20 @@ export default function RootLayout({
     <html lang="en" className={`${openSans.variable}`}>
       <ReduxProvider>
         <ReactQueryProvider>
-          <body>
-            <Banner />
-            <ToastContainer
-              position="top-center"
-              autoClose={3000}
-              hideProgressBar={true}
-              pauseOnHover
-              closeButton={false}
-              toastClassName={"rounded-2xl!"}
-            />
-            {children}
-          </body>
+          <PostHogProvider>
+            <body>
+              <Banner />
+              <ToastContainer
+                position="top-center"
+                autoClose={3000}
+                hideProgressBar={true}
+                pauseOnHover
+                closeButton={false}
+                toastClassName={"rounded-2xl!"}
+              />
+              {children}
+            </body>
+          </PostHogProvider>
         </ReactQueryProvider>
       </ReduxProvider>
     </html>
---
+++ src/context/Posthog.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from "posthog-js/react";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+
+const isProd = process.env.NODE_ENV === "production";
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (isProd) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+        api_host: "/ingest",
+        person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      });
+    }
+  }, []);
+
+  if (!isProd) {
+    return <>{children}</>;
+  }
+
+  return (
+    <PHProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PHProvider>
+  );
+}
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+
+  // Track pageviews
+  useEffect(() => {
+    if (pathname && posthog && isProd) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + "?" + searchParams.toString();
+      }
+
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
+
+// Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
+// from de-opting the whole app into client-side rendering
+// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
+function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  );
+}
