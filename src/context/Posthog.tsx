37520582ea3b2ab6ec58910452b2ed2ea1ae5d42
Sahil Yadav <10sahil11@gmail.com>
2025-04-30 02:20:33 +0530

    test

--- src/context/Posthog.tsx
+++ src/context/Posthog.tsx
@@ -15,6 +15,10 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
         person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
         capture_pageview: false, // Disable automatic pageview capture, as we capture manually
       });
+    } else {
+      console.warn(
+        "PostHog not initialized: missing key or not in production."
+      );
     }
   }, []);
 
