4b1b1468a8526391c831ebf5e0f2389e432c1a20
Sahil Yadav <10sahil11@gmail.com>
2025-04-30 17:18:32 +0530

    improve typing

--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -8,9 +8,7 @@ type Route = {
   isSideBar?: boolean;
 };
 
-type Routes = { [k: string]: Route };
-
-export const routes: Routes = {
+export const routes = {
   signIn: {
     pathname: "/sign-in",
     label: "Sign In",
@@ -31,22 +29,24 @@ export const routes: Routes = {
     icon: <GoPlus className="text-[20px]" strokeWidth={0.4} />,
     isSideBar: true,
   },
+  results: {
+    pathname: "/polls/{id}",
+    label: "Poll Info",
+    isSideBar: false,
+  },
   settings: {
     pathname: "/settings",
     label: "Settings",
     icon: <RiSettings4Line className="text-[20px]" strokeWidth={0.1} />,
     isSideBar: true,
   },
-} as const;
+} satisfies Record<string, Route>;
 
-const sidebarRoutes = Object.entries(routes).reduce((acc, [key, route]) => {
-  if (route.isSideBar) {
-    acc[key] = route;
+export const sidebarRoutesArray = Object.values(routes).filter(
+  (route: Route): route is Route & { isSideBar: true } => {
+    return route.isSideBar === true;
   }
-  return acc;
-}, {} as Routes);
-
-export const sidebarRoutesArray = Object.values(sidebarRoutes);
+);
 
 export const getRoute = (pathname: string): Route | undefined => {
   return Object.values(routes).find((route) => route.pathname === pathname);
